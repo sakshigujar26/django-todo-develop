f3c0ab348e0ae7f0189d277c1eacb1440d951bbe
sakshigujar26 <155972316+sakshigujar26@users.noreply.github.com>
2025-04-24 09:42:14 +0530

    Update settings.py

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todo-app.onrender.com']
+ALLOWED_HOSTS = ['todo-app-mdh8.onrender.com']
 
 
 # Application definition
